de7a96b8c1265b087220571bf61efd6689f923d9
serial-projects <120066091+serial-projects@users.noreply.github.com>
2023-03-24 19:35:10 -0300

    Update build.sh
    fix linking problem (there is no ./bin directory).

--- build.sh
+++ build.sh
@@ -3,12 +3,16 @@ CC="g++"
 CXXFLAGS="-g -O2 -march=native"
 SRC="./src/*.cpp"
 TGT="./bin/sol"
-compile(){
-    COMMAND="$CC $SRC -o $TGT $CXXFLAGS"
-    echo "[@]: $COMMAND" ; $COMMAND
-    if [[ $? -ne 0 ]]; then
-        echo "[!] Failed to compile!"
-        exit 1
-    fi
+prepare(){
+	[[ -d "./bin" ]] && return || mkdir ./bin
 }
+compile(){
+	COMMAND="$CC $SRC -o $TGT $CXXFLAGS"
+	echo "[@]: $COMMAND" ; $COMMAND
+	if [[ $? -ne 0 ]]; then
+		echo "[!] Failed to compile!"
+		exit 1
+	fi
+} 
+prepare
 compile
